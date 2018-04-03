fe7aa22cc2abaa0f6136ac792f921555d6b23977
Daras Monkey <davidariza1994@gmail.com>
2018-04-03 20:17:39 +0200

    Este es el bueno

--- sources/MC-1.rkt
+++ sources/MC-1.rkt
@@ -2,26 +2,42 @@
 
 (define (suscesores num_op nodo_actual)
   (let* (;Declaramos las variables para las operacionesde trasicion que son posibles.
-         (op_transicion (list (list 0 1 1  0 -1)  ; o1 = 1 canival se monata en la barca y no vuelve nadie.
-                              (list 1 0 1 -1  0)  ; o2 = 1 misionero se monta en la barca y no vuelve nadie.
-                              (list 2 0 1 -2  0)  ; o3 = 2 misioneros se montan en la barca y vuelve 1 misionero.
-                              (list 0 2 1  0 -2)  ; o4 = 2 canibales se montan en la barca y vuelve 1 canival.
-                              (list 1 0 1 -1 -1)  ; o5 = 1 misionero y 1 canival se montan y vuelve 1 canival en la barca.                              
+         (op_transicion (list (list 0 1 1  0 -1)  ; o1 = 1 canival se monata en la barca.
+                              (list 1 0 1 -1  0)  ; o2 = 1 misionero se monta en la barca.
+                              (list 2 0 1 -2  0)  ; o3 = 2 misioneros se montan en la barca.
+                              (list 0 2 1  0 -2)  ; o4 = 2 canibales se montan en la barca.
+                              (list 1 1 1 -1 -1)  ; o5 = 1 misionero y 1 canival se montan en la barca.
                          ))
          (new_sucesor   (list null ))
         )
     ;Declaramos las expresiones a usar.
     (cond
       [(< num_op (length op_transicion))
-        (set! new_sucesor (map (lambda (l1 l2) (+ l1 l2)) (list-ref op_transicion num_op) nodo_actual))
-        (printf " - Betta Sucesor ida a Y: ~v \n" new_sucesor)
-        (if (and (>= (list-ref new_sucesor 4) 0)    (>= (list-ref new_sucesor 3) 0) (eq? (apply + new_sucesor) 7)
-                 (>= (list-ref new_sucesor 3)       (list-ref new_sucesor 4))   ; validamos que Mx >= Cx en la orilla de partida.
-                 (>= (+ (list-ref new_sucesor 0) 1) (list-ref new_sucesor 1)))  ; validamos que My >= Cy en la orilla de partida.
-            (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
-            (suscesores (+ num_op 1) nodo_actual)
-        )]
-      [else empty] ; Finaliza la construcion de los sucesores de ida.
+         (set! new_sucesor (map (lambda (l1 l2) (+ l1 l2)) (list-ref op_transicion num_op) nodo_actual))
+         (printf " - Betta Sucesor ida a Y: ~v \n" new_sucesor)
+         (if (and (>= (list-ref new_sucesor 4) 0) (>= (list-ref new_sucesor 3) 0) (>= (list-ref new_sucesor 0) 0) (>= (list-ref new_sucesor 1) 0) (equal? (apply + new_sucesor) 7))
+              (cond
+                [(and (equal? num_op 0) (or (>= (list-ref new_sucesor 0) (+ (list-ref new_sucesor 1)) 1) (equal? (list-ref new_sucesor 0) 0)))
+                 (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
+                 ]
+                [(and (equal? num_op 3) (or (>= (list-ref new_sucesor 0) (+ (list-ref new_sucesor 1)) 2) (equal? (list-ref new_sucesor 0) 0)))
+                 (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
+                 ]
+                [(and (equal? num_op 1) (and (or (>= (list-ref new_sucesor 3) (+ (list-ref new_sucesor 4)) 1) (equal? (list-ref new_sucesor 3) 1)) (>= (list-ref new_sucesor 0) (- (list-ref new_sucesor 1)) 1)))
+                 (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
+                 ]
+                [(and (equal? num_op 2) (and (or (>= (list-ref new_sucesor 3) (+ (list-ref new_sucesor 4)) 2) (equal? (list-ref new_sucesor 3) 2)) (>= (list-ref new_sucesor 0) (- (list-ref new_sucesor 1)) 2)))
+                 (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
+                 ]
+                [(and (equal? num_op 4) (>= (list-ref new_sucesor 0) (list-ref new_sucesor 1)))
+                 (append (suscesores (+ num_op 1) nodo_actual) (cons new_sucesor '()))
+                 ]
+                [else (suscesores (+ num_op 1) nodo_actual)] 
+             )
+             (suscesores (+ num_op 1) nodo_actual)
+          )
+      ]
+      [else empty] ; Finaliza la construcion de los sucesores.
     )
   )
 )
